4f010bc4f91815370bb49970dbded1607aad0de6
北京-沈煜 <shenyu1@jd.com>
2018-12-01 14:28:06 +0800

    封装解析算法

--- 剑来/new/jianlai.py
+++ 剑来/new/jianlai.py
@@ -5,17 +5,22 @@ import simplejson
 
 
 
-class Jianlai(object):
-	"""总管，剑来"""
+
+class ZongHengSpider(object):
+	"""起点爬目录，格式一致, 以 剑来，总管 为例"""
+	def __init__(self,url,savename):
+		self.url = url
+		self.savename = savename
+
 	def get_save_csv(self):
-		respose = requests.get('http://book.zongheng.com/showchapter/672340.html').text
+		respose = requests.get(self.url).text
 		pattern = re.compile('title="(.*?)">')
 		"""
-        <li data-rid="6"><a href="//vipreader.qidian.com/chapter/1010496369/392128365" target="_blank" data-eid="qd_G55" data-cid="//vipreader.qidian.com/chapter/1010496369/392128365" title="首发时间：2017-12-03 20:00:00 章节字数：2114">第二十九章且看山道来一鬼</a>              
-            <em class="iconfont ">&#xe63c;</em>
-        </li>           
-        """
-		with open('jianlai.csv','w', encoding = 'utf-8-sig') as f:
+		<li data-rid="6"><a href="//vipreader.qidian.com/chapter/1010496369/392128365" target="_blank" data-eid="qd_G55" data-cid="//vipreader.qidian.com/chapter/1010496369/392128365" title="首发时间：2017-12-03 20:00:00 章节字数：2114">第二十九章且看山道来一鬼</a>              
+			<em class="iconfont ">&#xe63c;</em>
+		</li>           
+		"""
+		with open(self.savename,'w', encoding = 'utf-8-sig') as f:
 			f_csv = csv.writer(f)
 			for content in pattern.findall(respose):
 				# 第十六章 休想 字数：3672 更新时间：2017-06-17 23:56 
@@ -29,24 +34,23 @@ class Jianlai(object):
 					time = content[time_l:time_r]
 					limit = '2018-01-01 00:00'
 					if  time > limit:
-						print('enter')
 						arr = [content[:name_r],content[num_l:num_r],time]
-						# print(arr)
 						f_csv.writerow(arr)
 
 
-class Maoni(object):
-	"""猫腻，大道朝天"""
-	def __init__(self):
-		pass
+class QidianSpider(object):
+	"""起点爬目录，格式一致，以 猫腻，大道朝天  为例"""
+	def __init__(self,url,savename):
+		self.url = url
+		self.savename = savename
 		
 	def get_save_csv(self):
-		respose = requests.get('https://book.qidian.com/info/1010496369#Catalog').text
+		respose = requests.get(self.url).text
 		pattern = re.compile('title="(.*?)">(.*?)</a>')
- 		# <li data-rid="31"><a href="//read.qidian.com/chapter/_h17RCSkeXScikCo3ZPkrg2/gyIJWIpOlmzwrjbX3WA1AA2" target="_blank" data-eid="qd_G55" data-cid="//read.qidian.com/chapter/_h17RCSkeXScikCo3ZPkrg2/gyIJWIpOlmzwrjbX3WA1AA2" 
- 		# title="首发时间：2017-10-29 20:00:00 章节字数：2016">第三十章一朵奇葩入云来</a></li>
+		# <li data-rid="31"><a href="//read.qidian.com/chapter/_h17RCSkeXScikCo3ZPkrg2/gyIJWIpOlmzwrjbX3WA1AA2" target="_blank" data-eid="qd_G55" data-cid="//read.qidian.com/chapter/_h17RCSkeXScikCo3ZPkrg2/gyIJWIpOlmzwrjbX3WA1AA2" 
+		# title="首发时间：2017-10-29 20:00:00 章节字数：2016">第三十章一朵奇葩入云来</a></li>
 
-		with open('maoni.csv','w', encoding = 'utf-8-sig') as f:
+		with open(self.savename,'w', encoding = 'utf-8-sig') as f:
 			f_csv = csv.writer(f)
 			for content in pattern.findall(respose):
 				# ('首发时间：2017-10-15 09:44:53 章节字数：2536', '第一章 三千里禁')
@@ -61,41 +65,19 @@ class Maoni(object):
 						f_csv.writerow(arr)
 
 
-class Sanshao(object):
-	"""三少，斗罗大陆III龙王传说,8月24日之后，大龟甲师，11月22日之后，斗罗大陆外传唐门英雄传"""
-	def __init__(self):
-		pass
+			
+class QidianParseJsonFile(object):
+	"""起点抓取的json，解析存储，以 辰东，圣墟 为例"""
+	def __init__(self,jsonname,savename):
+		self.jsonname = jsonname
+		self.savename = savename
 		
 	def get_save_csv(self):
-		respose = requests.get('https://book.qidian.com/info/3681560#Catalog').text
-		pattern = re.compile('title="(.*?)">(.*?)</a>')
-		  # <li data-rid="17"><a href="//read.qidian.com/chapter/ndXhlu2kOVl51l-ULjeaVw2/01cS72bfgXpp4rPq4Fd4KQ2" target="_blank" data-eid="qd_G55" data-cid="//read.qidian.com/chapter/ndXhlu2kOVl51l-ULjeaVw2/01cS72bfgXpp4rPq4Fd4KQ2" 
-		  # title="首发时间：2018-08-28 07:00:00 章节字数：3205">第十七章 王老虎看病</a>
-
-		with open('sanshao1.csv','w', encoding = 'utf-8-sig') as f:
-			f_csv = csv.writer(f)
-			for content in pattern.findall(respose):
-				print(content)
-				# ('首发时间：2017-10-15 09:44:53 章节字数：2536', '第一章 三千里禁')
-				if content[0].startswith('首发时间') and content[1].startswith('第'):
-					print(content)
-					time_num_str = content[0]
-					time = time_num_str[5:24]
-					num_l = time_num_str.index('章节字数：') + 5
-					arr = [content[1], time_num_str[num_l:len(time_num_str)], time]
-					f_csv.writerow(arr)
-
-            
-class Chendong(object):
-	"""辰东，圣墟"""
-	def __init__(self):
-		pass
-		
-	def get_save_csv(self):
-		str_content = open('chendong.json').read()
+		str_content = open(self.jsonname).read()
 		dict_content = simplejson.loads(str_content)
 		vs = dict_content['data']['vs']
-		with open('chendong.csv','w', encoding = 'utf-8-sig') as f:
+
+		with open(self.savename,'w', encoding = 'utf-8-sig') as f:
 			f_csv = csv.writer(f)
 			for x in vs:
 				cs = x['cs']
@@ -107,16 +89,19 @@ class Chendong(object):
 
 	
 if __name__ == '__main__':
-	# jianlai = Jianlai()
+	# 总管，剑来
+	# jianlai = ZongHengSpider('http://book.zongheng.com/showchapter/672340.html','jianlai.csv')
 	# jianlai.get_save_csv()
 
-	# maoni = Maoni()
+
+	# 猫腻，大道朝天
+	# maoni = QidianSpider('https://book.qidian.com/info/1010496369#Catalog','maoni.csv')
 	# maoni.get_save_csv()
 
-	# sanshao = Sanshao()
-	# sanshao.get_save_csv()
+	# 辰东，圣墟
+	# chendong = QidianParseJsonFile('chendong.json','chendong.csv')
+	# chendong.get_save_csv()
 
 
 
-	chendong = Chendong()
-	chendong.get_save_csv()
+
