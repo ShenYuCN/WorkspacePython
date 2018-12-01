53c714f7ec2bc12bce1893cd705297b64a068d30
北京-沈煜 <shenyu1@jd.com>
2018-12-01 15:19:17 +0800

    可视化分析封装处理

--- 剑来/new/analyse.py
+++ 剑来/new/analyse.py
@@ -5,52 +5,83 @@ import csv
 
 
 
+class Analyse(object):
+	"""网络小说作者2018年更新分析"""
 
-def sum_year_count(name):
-	sum = 0
-	with open(name) as f:
-		f_reader = csv.reader(f)
-		for row  in f_reader:
-			sum += int(row[1])
-	print(sum)
-	return sum
+	allinfo = [
+				('辰东-圣墟','chendong.csv'),
+				('总管-剑来','jianlai.csv'),
+				('猫腻-大道朝天','maoni.csv')
+				]
+
+	title = '贴吧@桑桑好桑心'
+	subtitle = '数据整理自起点和纵横，截止2018.11.30 21:00'
+
+	# def __init__(self, arg):
+	# 	super(Analyse, self).__init__()
+	# 	self.arg = arg
+
+	def __init__(self):
+		pass
+
+	def sum_year_count(self,filename):
+		""" 一年的更新字数  """
+		sum = 0
+		with open(filename) as f:
+			f_reader = csv.reader(f)
+			for row  in f_reader:
+				sum += int(row[1])
+		print(sum)
+		return sum
+
+
+	def render_average_day(self):
+		"""2018年平均每天更新字数"""
+		# configure(global_theme='essos')
+
+		bar = Bar(self.title,self.subtitle)
+		# bar.use_theme('dark')
+
+		authors = []
+		one_years_all = []
+		for info in self.allinfo:
+			authors.append(info[0])
+			one_years_all.append(self.sum_year_count(info[1]))
+
+		every_day = [int(x / (365 - 31)) for x in one_years_all]
+		print(authors,every_day)
+
+		bar.add("2018年平均每天更新字数",
+				authors,
+				every_day,
+				is_label_show=True,
+				label_pos='inside'
+				)
+		bar.render('2018年平均每天更新字数.html')
+
+	def render_all_year(self):
+		bar = Bar(self.title,self.subtitle)
+		authors = []
+		one_years_all = []
+		for info in self.allinfo:
+			authors.append(info[0])
+			one_years_all.append(self.sum_year_count(info[1]))
+
+		one_years_all = [int(x / 10000) for x in one_years_all]
+		bar.add('2018年总更新字数,单位（万字）',
+				authors,
+				one_years_all,
+				is_label_show=True,
+				label_pos='inside'
+				)
+		bar.render('2018年总更新字数.html')
+
+
 
 if __name__ == '__main__':
-	sum_count_jianlai = sum_year_count('jianlai.csv')
-	sum_count_chendong = sum_year_count('chendong.csv')
-	sum_count_maoni = sum_year_count('maoni.csv')
+	configure(global_theme='dark')
 
-	configure(global_theme = 'dark')
-	bar = Bar("贴吧@桑桑好桑心", "数据整理自起点和纵横，截止2018.11.30 21:00")
-	# bar.use_theme('essos')
-
-	# bar.add("2018年平均每天更新字数", 
-	# 	["辰东", "总管", "猫腻"], 
-	# 	[count_jianlai, count_chendong, count_maoni],
-	# 	is_label_show=True
-	# 	)
-
-
-	authers = ["辰东", "总管", "猫腻"]
-	sum_year_original = [sum_count_chendong,sum_count_jianlai,sum_count_maoni]
-	print(sum_year_original)
-	average_day = [int(x / (365 - 31)) for x in sum_year_original]
-	sum_year = [int(x / 10000) for x in sum_year_original]
-	print(sum_year)
-
-	# bar.add(
-	# 	"2018年平均每天更新字数", 
-	# 	authers,
-	# 	average_day, 
-	# 	is_label_show=True
-	# )
-	bar.add(
-		"2018年总共更新字数,单位（万字）",
-		authers,
-		sum_year,
-		is_label_show=True,
-		is_stack=True,
-		label_pos="inside"
-	)
-
-	bar.render()    # 生成本地 HTML 文件
+	analyse = Analyse()
+	analyse.render_average_day()
+	analyse.render_all_year()
+	
